7c4fc1cb0975edc3d9305e33e947dc86a3173bd3
Aya Tawfik <aya.tawfik98@gmail.com>
2020-04-16 19:18:00 +0200

    aya

--- FacultyEventPlanner/Form1.cs
+++ FacultyEventPlanner/Form1.cs
@@ -34,5 +34,10 @@ namespace FacultyEventPlanner
         {
             OracleHelper.closeConnection();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ////aya///
+        }
     }
 }
