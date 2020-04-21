370cff7a9331c23ef01294db33d710203dc097e7
Haneen Tamer <haneen.tamer@cis.asu.edu.eg>
2020-04-21 23:22:43 +0200

    MERGE MESS FIXED

--- FacultyEventPlanner/AdminHome.Designer.cs
+++ FacultyEventPlanner/AdminHome.Designer.cs
@@ -58,12 +58,10 @@
             this.Add_Member_button.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Add_Member_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Add_Member_button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-<<<<<<< HEAD
+
             this.Add_Member_button.Location = new System.Drawing.Point(133, 70);
             this.Add_Member_button.Margin = new System.Windows.Forms.Padding(4);
-=======
-            this.Add_Member_button.Location = new System.Drawing.Point(100, 57);
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.Add_Member_button.Name = "Add_Member_button";
             this.Add_Member_button.Size = new System.Drawing.Size(117, 52);
             this.Add_Member_button.TabIndex = 0;
@@ -76,12 +74,10 @@
             this.View_E_Button.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.View_E_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.View_E_Button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-<<<<<<< HEAD
+
             this.View_E_Button.Location = new System.Drawing.Point(459, 127);
             this.View_E_Button.Margin = new System.Windows.Forms.Padding(4);
-=======
-            this.View_E_Button.Location = new System.Drawing.Point(344, 103);
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.View_E_Button.Name = "View_E_Button";
             this.View_E_Button.Size = new System.Drawing.Size(134, 53);
             this.View_E_Button.TabIndex = 1;
@@ -94,12 +90,10 @@
             this.View_V_Button.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.View_V_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.View_V_Button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-<<<<<<< HEAD
+
             this.View_V_Button.Location = new System.Drawing.Point(717, 127);
             this.View_V_Button.Margin = new System.Windows.Forms.Padding(4);
-=======
-            this.View_V_Button.Location = new System.Drawing.Point(538, 103);
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.View_V_Button.Name = "View_V_Button";
             this.View_V_Button.Size = new System.Drawing.Size(127, 52);
             this.View_V_Button.TabIndex = 2;
@@ -112,12 +106,10 @@
             this.View_Jobs.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.View_Jobs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.View_Jobs.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-<<<<<<< HEAD
+
             this.View_Jobs.Location = new System.Drawing.Point(1072, 70);
             this.View_Jobs.Margin = new System.Windows.Forms.Padding(4);
-=======
-            this.View_Jobs.Location = new System.Drawing.Point(804, 57);
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.View_Jobs.Name = "View_Jobs";
             this.View_Jobs.Size = new System.Drawing.Size(116, 52);
             this.View_Jobs.TabIndex = 3;
@@ -138,12 +130,10 @@
             this.Ls_start_Time,
             this.Ls_Date,
             this.L_name});
-<<<<<<< HEAD
+
             this.dataGridView1.Location = new System.Drawing.Point(36, 260);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
-=======
-            this.dataGridView1.Location = new System.Drawing.Point(27, 211);
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
@@ -211,12 +201,10 @@
             // 
             this.Combo_Box_of_DEP.BackColor = System.Drawing.SystemColors.MenuBar;
             this.Combo_Box_of_DEP.FormattingEnabled = true;
-<<<<<<< HEAD
+
             this.Combo_Box_of_DEP.Location = new System.Drawing.Point(989, 215);
             this.Combo_Box_of_DEP.Margin = new System.Windows.Forms.Padding(4);
-=======
-            this.Combo_Box_of_DEP.Location = new System.Drawing.Point(742, 175);
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.Combo_Box_of_DEP.Name = "Combo_Box_of_DEP";
             this.Combo_Box_of_DEP.Size = new System.Drawing.Size(208, 21);
             this.Combo_Box_of_DEP.TabIndex = 5;
@@ -304,12 +292,11 @@
             this.Controls.Add(this.View_V_Button);
             this.Controls.Add(this.View_E_Button);
             this.Controls.Add(this.Add_Member_button);
-<<<<<<< HEAD
+
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
-=======
->>>>>>> 965597bd63a11576e8be1f5a37951341067eabbb
+
             this.Name = "AdminHome";
             this.Text = "AdminHome";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Admin_home);
